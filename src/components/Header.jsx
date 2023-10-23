4cddba61d9cb4d91c154634f7072ba55c8960d3f
Gustavo Passarella <gustavo.passarella@hotmail.com>
2023-10-23 17:17:33 +0100

    react intro

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Nav from "./Nav";
+
+export default class Header extends Component {
+	render() {
+		return (
+			<>
+				<h2>This is the nav</h2>
+				<Nav />
+			</>
+		);
+	}
+}
